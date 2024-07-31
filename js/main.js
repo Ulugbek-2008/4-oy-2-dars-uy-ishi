51c4b847f11215207e20a94cecd95d5cb01964aa
Ulugbek-2008 <u20081219@gmail.com>
2024-07-31 23:50:04 +0500

    Add js file

--- js/main.js
+++ js/main.js
@@ -1,199 +1,189 @@
 
 
+// 1-sinf ishi
+let AlisherMoney = prompt("Alisher aka qancha puliz bor?")
 
-// let AlisherMoney = prompt("Alisher aka qancha puliz bor?")
+let ticket = 500 // $
+let hotel = 250 // $
+let travel = 120 // Euro
 
-// let ticket = 500 // $
-// let hotel = 250 // $
-// let travel = 120 // Euro
+let currencyDollor = 11000.34 // $
+let currencyEuro = 12354.03  // Euro
 
-// let currencyDollor = 11000.34 // $
-// let currencyEuro = 12354.03  // Euro
+let totalPriceDollor = (ticket + hotel) * currencyDollor // Sum
+let totalPriceEuro = travel * currencyEuro // Sum
+let resultPrice = totalPriceDollor + totalPriceEuro // All Charges money to SUM
 
-// let totalPriceDollor = (ticket + hotel) * currencyDollor // Sum
-// let totalPriceEuro = travel * currencyEuro // Sum
-// let resultPrice = totalPriceDollor + totalPriceEuro // All Charges money to SUM
-
-// if(AlisherMoney >= resultPrice){
-//     alert('Borishinggiz mumkin');
-// }
-// else{
-//     alert("Pulinggiz kam , ozroq kuting");
-// }
-
-
+if(AlisherMoney >= resultPrice){
+    alert('Borishinggiz mumkin');
+}
+else{
+    alert("Pulinggiz kam , ozroq kuting");
+}
 
 
 
 
-// let age = prompt("Yosh kiriting")
+// 2-sinf ishi
 
-// if(age < 0){
-//     alert("Musbat son kiriting!")
-// }
-// else if(age > 0 && age <= 10){
-//     alert("Yosh bola");
-// }
-// else if(age >= 11 && age <= 15){
-//     alert("O'smir");
-// }
-// else if(age >= 16 && age <= 25){
-//     alert("Boydoq");
-// }
-// else if(age >= 26 && age <= 40){
-//     alert("Kata yoshli odam ");
-// }
-// else if(age >= 41 && age <= 70){
-//     alert("Doda , (Otaxon)");
-// }
-// else if(age > 70){
-//     alert("Umringgiz uzoq bolsin");
-// }
-// else{
-//     alert("Son kiriting !");
-// }
+let age = prompt("Yosh kiriting")
 
+if(age < 0){
+    alert("Musbat son kiriting!")
+}
+else if(age > 0 && age <= 10){
+    alert("Yosh bola");
+}
+else if(age >= 11 && age <= 15){
+    alert("O'smir");
+}
+else if(age >= 16 && age <= 25){
+    alert("Boydoq");
+}
+else if(age >= 26 && age <= 40){
+    alert("Kata yoshli odam ");
+}
+else if(age >= 41 && age <= 70){
+    alert("Doda , (Otaxon)");
+}
+else if(age > 70){
+    alert("Umringgiz uzoq bolsin");
+}
+else{
+    alert("Son kiriting !");
+}
 
-// let number = prompt("Son kiriting")
+let number = prompt("Son kiriting")
 
-// if(number < 0){
-//   alert("Musbat son kiriting")
-// }
-// else if(number > 0 && number <= 9){
-//   alert("Bir xonali son");
-// }
-// else if(number >= 10 && number <= 99){
-//   alert("Ikki xonali son");
-// }
-// else if(number >= 100 && number <= 999){
-//   alert("Uch xonali son");
-// }
-// else if(number >= 1000 && number <= 9999){
-//   alert("Tort xonali son");
-// }
-
-
-
-
-// let Number = prompt("Son kiriting!")
-
-// if(Number < 0){
-//   alert("Musbat son kiriting")
-// }
-// else if(Number % 2 == 0){
-//   alert("Juft son");
-// }
-// else if(Number % 2 != 0){
-//   alert("Toq son");
-// }
-
-
-// let NUMBER = prompt("Son kiriting!")
-
-// if(NUMBER < 0){
-//   alert("Musbat son kiriting")
-// }
-// else if(NUMBER % 2 == 0 && NUMBER > 0 && NUMBER <= 9){
-//   alert("Bir xonali juft son");
-// }
-// else if(NUMBER % 2 == 0 &&  NUMBER > 10 && NUMBER <= 99){
-//   alert("Ikki xonali juft son");
-// }
-// else if(NUMBER % 2 == 0 && NUMBER >= 100 && NUMBER <= 999){
-//   alert("Uch xonali juft son");
-// }
-
-// else if(NUMBER % 2 == 0 && NUMBER >= 1000 && NUMBER <= 9999){
-//   alert("Tort xonali juft son");
-// }
-// else if(NUMBER % 2 != 0 &&  NUMBER > 0 && NUMBER <= 9){
-//   alert("Bir xonali toq son");
-// }
-// else if(NUMBER % 2 != 0 &&  NUMBER >= 10 && NUMBER <= 99){
-//   alert(" Ikki xonali toq son");
-// }
-// else if(NUMBER % 2 != 0 &&  NUMBER >= 100 && NUMBER <= 999){
-//   alert("Uch xonali toq son");
-// }
-// else if(NUMBER % 2 != 0 &&  NUMBER >= 1000 && NUMBER <= 9999){
-//   alert("Tort xonali toq son");
-// }
+if(number < 0){
+  alert("Musbat son kiriting")
+}
+else if(number > 0 && number <= 9){
+  alert("Bir xonali son");
+}
+else if(number >= 10 && number <= 99){
+  alert("Ikki xonali son");
+}
+else if(number >= 100 && number <= 999){
+  alert("Uch xonali son");
+}
+else if(number >= 1000 && number <= 9999){
+  alert("Tort xonali son");
+}
 
 
 
+let Number = prompt("Son kiriting!")
 
-// let FizzBuzz = prompt("Raqam kiriting")
+if(Number < 0){
+  alert("Musbat son kiriting")
+}
+else if(Number % 2 == 0){
+  alert("Juft son");
+}
+else if(Number % 2 != 0){
+  alert("Toq son");
+}
+
+let NUMBER = prompt("Son kiriting!")
+
+if(NUMBER < 0){
+  alert("Musbat son kiriting")
+}
+else if(NUMBER % 2 == 0 && NUMBER > 0 && NUMBER <= 9){
+  alert("Bir xonali juft son");
+}
+else if(NUMBER % 2 == 0 &&  NUMBER > 10 && NUMBER <= 99){
+  alert("Ikki xonali juft son");
+}
+else if(NUMBER % 2 == 0 && NUMBER >= 100 && NUMBER <= 999){
+  alert("Uch xonali juft son");
+}
+
+else if(NUMBER % 2 == 0 && NUMBER >= 1000 && NUMBER <= 9999){
+  alert("Tort xonali juft son");
+}
+else if(NUMBER % 2 != 0 &&  NUMBER > 0 && NUMBER <= 9){
+  alert("Bir xonali toq son");
+}
+else if(NUMBER % 2 != 0 &&  NUMBER >= 10 && NUMBER <= 99){
+  alert(" Ikki xonali toq son");
+}
+else if(NUMBER % 2 != 0 &&  NUMBER >= 100 && NUMBER <= 999){
+  alert("Uch xonali toq son");
+}
+else if(NUMBER % 2 != 0 &&  NUMBER >= 1000 && NUMBER <= 9999){
+  alert("Tort xonali toq son");
+}
 
 
+let FizzBuzz = prompt("Raqam kiriting")
 
-// if( FizzBuzz % 3 == 0){
-//   alert("Fizz");
-// }
-// else if( FizzBuzz % 5 == 0){
-//   alert("Buzz");
-// }
-// else if( FizzBuzz % 3 == 0 && FizzBuzz % 5 == 0){
-//   alert("FizzBuzz");
-// }
-// else if( FizzBuzz % 3 != 0 && FizzBuzz % 5 != 0){
-//   alert("Boshqa son kiriting");
-// }
-
-
-
-// let numeral = prompt("Son kiriting")
+if( FizzBuzz % 3 == 0){
+  alert("Fizz");
+}
+else if( FizzBuzz % 5 == 0){
+  alert("Buzz");
+}
+else if( FizzBuzz % 3 == 0 && FizzBuzz % 5 == 0){
+  alert("FizzBuzz");
+}
+else if( FizzBuzz % 3 != 0 && FizzBuzz % 5 != 0){
+  alert("Boshqa son kiriting");
+}
 
 
-//  if( numeral > 0 && numeral >=1 && numeral <=9 && numeral % 2 == 0){
-//   alert("Bir xonali juft musbat son");
-//  }
-//  else if( numeral > 0 && numeral >=1 && numeral <=9 && numeral % 2 != 0){
-//   alert("Bir xonali toq musbat son");
-//  }
-//  else if( numeral > 0 && numeral >= 10 && numeral <= 99 && numeral % 2 == 0){
-//   alert("ikki xonali juft musbat son");
-//  }
-//  else if( numeral > 0 && numeral >= 10 && numeral <= 99 && numeral % 2 != 0){
-//   alert("ikki xonali toq musbat son");
-//  }
-//  else if( numeral > 0 && numeral >= 100 && numeral <= 999 && numeral % 2 == 0){
-//   alert("uch xonali juft musbat son");
-//  }
-//  else if( numeral > 0 && numeral >= 100 && numeral <= 999 && numeral % 2 != 0){
-//   alert("uch xonali toq musbat son");
-//  }
-//  else if( numeral > 0 && numeral % 2 == 0){
-//   alert("Musbat juft son");
-//  }
-//  else if( numeral > 0 && numeral % 2 != 0 ){
-//   alert("Musbat toq son");
-//  }
+let numeral = prompt("Son kiriting")
 
-//  else if( numeral < 0 && numeral >=1 && numeral <=9 && numeral % 2 == 0){
-//   alert("Bir xonali juft manfiy son");
-//  }
-//  else if( numeral < 0 && numeral >=1 && numeral <=9 && numeral % 2 != 0){
-//   alert("Bir xonali toq manfiy son");
-//  }
-//  else if( numeral < 0 && numeral >= 10 && numeral <= 99 && numeral % 2 == 0){
-//   alert("ikki xonali juft manfiy son");
-//  }
-//  else if( numeral < 0 && numeral >= 10 && numeral <= 99 && numeral % 2 != 0){
-//   alert("ikki xonali toq manfiy son");
-//  }
-//  else if( numeral < 0 && numeral >= 100 && numeral <= 999 && numeral % 2 == 0){
-//   alert("uch xonali juft manfiy son");
-//  }
-//  else if( numeral < 0 && numeral >= 100 && numeral <= 999 && numeral % 2 != 0){
-//   alert("uch xonali toq manfiy son");
-//  }
-//  else if( numeral < 0 && numeral % 2 == 0){
-//   alert("manfiy juft son");
-//  }
-//  else if( numeral < 0 && numeral % 2 != 0 ){
-//   alert("manfiy toq son");
-//  }
+ if( numeral > 0 && numeral >=1 && numeral <=9 && numeral % 2 == 0){
+  alert("Bir xonali juft musbat son");
+ }
+ else if( numeral > 0 && numeral >=1 && numeral <=9 && numeral % 2 != 0){
+  alert("Bir xonali toq musbat son");
+ }
+ else if( numeral > 0 && numeral >= 10 && numeral <= 99 && numeral % 2 == 0){
+  alert("ikki xonali juft musbat son");
+ }
+ else if( numeral > 0 && numeral >= 10 && numeral <= 99 && numeral % 2 != 0){
+  alert("ikki xonali toq musbat son");
+ }
+ else if( numeral > 0 && numeral >= 100 && numeral <= 999 && numeral % 2 == 0){
+  alert("uch xonali juft musbat son");
+ }
+ else if( numeral > 0 && numeral >= 100 && numeral <= 999 && numeral % 2 != 0){
+  alert("uch xonali toq musbat son");
+ }
+ else if( numeral > 0 && numeral % 2 == 0){
+  alert("Musbat juft son");
+ }
+ else if( numeral > 0 && numeral % 2 != 0 ){
+  alert("Musbat toq son");
+ }
 
+ else if( numeral < 0 && numeral >=1 && numeral <=9 && numeral % 2 == 0){
+  alert("Bir xonali juft manfiy son");
+ }
+ else if( numeral < 0 && numeral >=1 && numeral <=9 && numeral % 2 != 0){
+  alert("Bir xonali toq manfiy son");
+ }
+ else if( numeral < 0 && numeral >= 10 && numeral <= 99 && numeral % 2 == 0){
+  alert("ikki xonali juft manfiy son");
+ }
+ else if( numeral < 0 && numeral >= 10 && numeral <= 99 && numeral % 2 != 0){
+  alert("ikki xonali toq manfiy son");
+ }
+ else if( numeral < 0 && numeral >= 100 && numeral <= 999 && numeral % 2 == 0){
+  alert("uch xonali juft manfiy son");
+ }
+ else if( numeral < 0 && numeral >= 100 && numeral <= 999 && numeral % 2 != 0){
+  alert("uch xonali toq manfiy son");
+ }
+ else if( numeral < 0 && numeral % 2 == 0){
+  alert("manfiy juft son");
+ }
+ else if( numeral < 0 && numeral % 2 != 0 ){
+  alert("manfiy toq son");
+ }
 
 
 
@@ -216,3 +206,23 @@
  else if( c < b < a && a < 10 && b < 10 && c < 10){
   alert(c + b + a);
  }
+
+
+
+let speed = prompt("Tezligingizni kiriting:")
+let point = 0
+if(speed >= 70){
+    point = (speed - 70) / 5
+    if(point > 12){
+        alert("Prava olindi")
+    }else{
+        alert("pointlar soni: " + point)
+    }
+}
+
+
+let year = prompt("tug'ilgan yilingizni kiriting:")
+let currentYear = 2024
+let Age = currentYear - year
+
+alert(age)
